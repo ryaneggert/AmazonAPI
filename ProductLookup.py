f699a581a6f2ab8573103097dce89d3c87b5ae68
RyanEggert <Ryan.Eggert@students.olin.edu>
2013-11-27 00:45:45 -0600

    Reading from text file & time looping

--- ProductLookup.py
+++ ProductLookup.py
@@ -5,58 +5,80 @@ import string
 import xlwt
 import time
 
-startTime = time.strftime('%c %z', time.localtime())
+
+def ASINLookup(ASIN, x, y):  # x is looping through ASINs. y for time
+    amazon = bottlenose.Amazon(
+        'AKIAI4I6EMMUJQXSGCFA', '/QTXbjXBMJk75UsOxNbAhRwkk0aVjPAKUla+ofaM',
+        'ryane-20')
+    try:
+        product = amazon.ItemLookup(
+            ItemId=ASIN, ResponseGroup='SalesRank,ItemAttributes')
+    except:
+        print "HTML/Internet Connection Error"
+        if y == 1:  # Only find category the first time        
+            Sheet.write(x + 1, 0, ASIN)  # Only write ASIN the first time
+            Sheet.write(x + 1, 1, "Internet Error")  # Only write category the first time
+        Sheet.write(x + 1, 1 + y, "Internet Error")
+
+    else:
+        Response = str(product)
+
+        SRPos = Response.find('</SalesRank>')
+
+        if y == 1:  # Only find category the first time
+            CgPos = Response.find('</ProductGroup>')
+
+            if CgPos == -1:  # If the program didn't find a category in the XML
+                Category = 'No Category Found'
+            else:
+                Cg = Response[CgPos - 40:CgPos]
+                Cat = Cg.split(">")
+                Category = Cat[-1]
+            print Category
+            Sheet.write(x + 1, 0, ASIN)  # Only write ASIN the first time
+            Sheet.write(x + 1, 1, Category)  # Only write category the first time
+
+        if SRPos == -1:  # If the program didn't find a sales rank in the XML
+            SalesRank = 'No Sales Rank Found'
+        else:
+            SR = Response[SRPos - 30:SRPos]
+            SalesRank = SR.translate(None, IsolateSalesRank)
+            # strip all chars, except for digits, leaving only the SalesRank
+            # behind
+        print SalesRank
+
+        try:
+            Sheet.write(x + 1, 1 + y, int(SalesRank))
+        except ValueError:
+            Sheet.write(x + 1, 1 + y, SalesRank)
+
+
+OrigTime = time.strftime('%c %z', time.localtime())
+
+ASINText = open('ASINTest.txt', 'r') 
+
+D=ASINText.read()
+print D
+ASINs= D.split('\n')
+
 
 wbk = xlwt.Workbook()
 Sheet = wbk.add_sheet('Product Sales Rank Data', cell_overwrite_ok=True)
 
 Sheet.write(0, 0, "ASIN")
 Sheet.write(0, 1, "Category")
-Sheet.write(0, 2, "Sales Rank" + startTime)
-
-
-def ASINLookup(ASIN, x, y):  # x is looping through ASINs. y for time
-    amazon = bottlenose.Amazon(
-        'AKIAI4I6EMMUJQXSGCFA', '/QTXbjXBMJk75UsOxNbAhRwkk0aVjPAKUla+ofaM',
-        'ryane-20')
-
-    product = amazon.ItemLookup(
-        ItemId=ASIN, ResponseGroup='SalesRank,ItemAttributes')
-    Response = str(product)
-
-    SRPos = Response.find('</SalesRank>')
-
-    if y == 1:  # Only find category the first time
-        CgPos = Response.find('</ProductGroup>')
-
-        if CgPos == -1:  # If the program didn't find a category in the XML
-            Category = 'No Category Found'
-        else:
-            Cg = Response[CgPos - 40:CgPos]
-            Cat = Cg.split(">")
-            Category = Cat[-1]
-        print Category
-        Sheet.write(x + 1, 0, ASIN)  # Only write ASIN the first time
-        Sheet.write(x + 1, 1, Category)  # Only write category the first time
-
-    if SRPos == -1:  # If the program didn't find a sales rank in the XML
-        SalesRank = 'No Rank Found'
-    else:
-        SR = Response[SRPos - 30:SRPos]
-        SalesRank = SR.translate(None, IsolateSalesRank)
-        # strip all chars, except for digits, leaving only the SalesRank
-        # behind
-    print SalesRank
-    Sheet.write(x + 1, 1 + y, SalesRank)
 
 IsolateSalesRank = string.ascii_letters + '<>/'
 IsolateCategory = '<>/'
 
-ASINs = ['B003R248Q0', 'B002S53CN2', 'B005XLN97M', 'B003BLQEFA',
-         'B004OFBJX4', 'B003S9VNWE', 'B007RDGJHG', 'B0002KZ4VI', 'B0038M5ILI']
+# ASINs = ['B003R248Q0', 'B002S53CN2', 'B005XLN97M', 'B003BLQEFA',
+#          'B004OFBJX4', 'B003S9VNWE', 'B007RDGJHG', 'B0002KZ4VI', 'B0038M5ILI']
 
-for j in range(1, 11):  # 11 is number of hours to check. First j always 1
+for j in range(1, 4):  # 11 is number of hours to check. First j always 1
+    startTime = time.strftime('%c %z', time.localtime())
+    Sheet.write(0, j + 1, "Sales Rank" + startTime)
     for i in range(1, len(ASINs) + 1):
         ASINLookup(ASINs[i - 1], i, j)
-        print "Searching Product " + str(i) + " of " + str(len(ASINs) + 1)
+        print "Searching Product " + str(i) + " of " + str(len(ASINs))
+        time.sleep(.5)
     wbk.save('AmazonSalesRankData.xls')  # ADD DATE TO FILENAME
--- SpreadsheetWriter.py
+++ SpreadsheetWriter.py
@@ -1,30 +1,52 @@
-import xlwt
-import time
+# # import xlwt
+# # import time
 
 
-def SSWrite(Names, Sheet):
-    for i in range(1, len(Names)+1):
-        Sheet.write(i, 0, Names[i-1])
-        print Names[i-1]
+# # def SSWrite(Names, Sheet):
+# #     for i in range(1, len(Names)+1):
+# #         Sheet.write(i, 0, Names[i-1])
+# #         print Names[i-1]
 
-wbk = xlwt.Workbook()
-Datasheet = wbk.add_sheet('Data', cell_overwrite_ok=True)
+# # wbk = xlwt.Workbook()
+# # Datasheet = wbk.add_sheet('Data', cell_overwrite_ok=True)
 
 
-startTime = time.strftime('%c %z', time.localtime())
-# Title
-Datasheet.write(0, 0, "ASIN")
-Datasheet.write(0, 1, "Category")
-Datasheet.write(0, 2, "Sales Rank" + startTime)
+# # # Title
+# # Datasheet.write(0, 0, "ASIN")
+# # Datasheet.write(0, 1, "Category")
+# # Datasheet.write(0, 2, "Sales Rank" + startTime)
 
-List = ['George', 'Hank', 'Bill', 'Larry', 'John', 'Tipperary', 'Harold']
+# # List = ['George', 'Hank', 'Bill', 'Larry', 'John', 'Tipperary', 'Harold']
 
-print len(List)
+# # print len(List)
 
-print range(1, 1+len(List))
+# # print range(1, 10)
 
-print List[5]
+# # print List[5]
 
-SSWrite(List, Datasheet)
+# import urllib2 as u
+# Response= u.urlopen('http://www.tutorialspoint.com/python/string_zfill.htm')
 
-wbk.save('TestExcel.xls')
+# Q = Response.read()
+
+# List = Q.split('<')
+
+# print List
+
+# # SSWrite(List, Datasheet)
+
+# # wbk.save('TestExcel.xls')
+
+# K=0
+# for i in range(1,20):
+#     K += 5
+#     print K
+#     print
+#     print
+
+f = open('ASINTest.txt', 'r') 
+
+D=f.read()
+print D
+G= D.split('\n')
+print G 
