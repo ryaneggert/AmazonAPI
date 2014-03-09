8e335677ec184ebc9e11eddb30373a2f08d5a404
RyanEggert <Ryan.Eggert@students.olin.edu>
2014-03-09 15:44:15 -0400

    More/Nicer Comments
    Additional, clarifying comments

--- ProductLookup.py
+++ ProductLookup.py
@@ -1,22 +1,27 @@
 # ProductLookup.py
 
-import bottlenose
+import bottlenose  # Amazon P.A. API bindings
 import string
-import xlwt
-import time
+import xlwt  # Write to MS Excel file
+import time  # Schedule data collection
 
 
 def ASINLookup(ASIN, x, y):  # x is looping through ASINs. y for time
     amazon = bottlenose.Amazon(
         'AKIAI4I6EMMUJQXSGCFA', '/QTXbjXBMJk75UsOxNbAhRwkk0aVjPAKUla+ofaM',
         'ryane-20')
+
+    # Rapid requests periodically fail. Code reattempts failed requests a set 
+    # number of times. This number has been tested, and it minimizes the number  
+    # of valueless returns while not devoting too much time to repeat requests.
     for attempts in range(1, 10):
         try:
-            product = amazon.ItemLookup(
-                ItemId=ASIN, ResponseGroup='SalesRank,ItemAttributes')
+            # Attempt API call
+            product = amazon.ItemLookup( 
+                ItemId=ASIN, ResponseGroup='SalesRank,ItemAttributes') 
         except:
-            print "HTML/Internet Connection Error"
-            time.sleep(.5)
+            print "HTML/Internet Connection Error" #Display error msg.
+            time.sleep(.5) #Wait before retrying
 
         else:
             Response = str(product)
@@ -52,9 +57,9 @@ def ASINLookup(ASIN, x, y):  # x is looping through ASINs. y for time
                 Sheet.write(x + 1, 1 + y, int(SalesRank))
             except ValueError:
                 Sheet.write(x + 1, 1 + y, SalesRank)
-            break
+            break #Leave loop on successful product loopkup process
     else:  # Multiple Attempts have failed
-        print "Damn."
+        print "Fiddlesticks."  # Oh bother.
         if y == 1:  # Only find category the first time
             Sheet.write(x + 1, 0, ASIN)  # Only write ASIN the first time
             # Only write category the first time
@@ -66,32 +71,35 @@ OrigTime = time.strftime('%c %z', time.localtime())
 FileStart = time.strftime('%m%d_%H%M', time.localtime())
 ASINText = open('ASINsFin.txt', 'r')
 
-RwIn = ASINText.read()
+RwIn = ASINText.read()  # Raw Input
 
-ASINs = RwIn.split('\n')
+ASINs = RwIn.split('\n') # Split into list of ASINs
 
 
-wbk = xlwt.Workbook()
+wbk = xlwt.Workbook()  # Create workbook object
 Sheet = wbk.add_sheet('Product Sales Rank Data', cell_overwrite_ok=True)
 
-Sheet.write(0, 0, "ASIN")
+#Column Headers
+Sheet.write(0, 0, "ASIN") 
 Sheet.write(0, 1, "Category")
 
 IsolateSalesRank = string.ascii_letters + '<>/'
 IsolateCategory = '<>/'
 
-
-for j in range(1, 37):  # 11 is number of hours to check. First j always 1
+RunT = 40 #Number of hours to check.
+for j in range(1, RunT+1):  # RunT+1 to make range() inclusive. First j always 1
     startTime = time.strftime('%c %z', time.localtime())
     Sheet.write(0, j + 1, "Sales Rank" + startTime)
     eTs = time.clock()
     for i in range(1, len(ASINs) + 1):
         ASINLookup(ASINs[i - 1], i, j)
         print "Searching Product " + str(i) + " of " + str(len(ASINs))
-        # time.sleep(.5)
-    wbk.save('AmazonSalesRankData' + FileStart + '.xls')
+        # time.sleep(.5) # Not needed. Pause only on fail condition
+
+    #Dynamically name file to prevent overwrite
+    wbk.save('AmazonSalesRankData' + FileStart + '.xls') 
     eTf = time.clock()
-    elapsedTime = eTf - eTs
+    elapsedTime = eTf - eTs  # Elapsed time of each loop iteration
     print 'Elapsed Time = ' + str(elapsedTime)
     if elapsedTime < 3600:
         print("Finished Hour " + str(j) + " at " +
