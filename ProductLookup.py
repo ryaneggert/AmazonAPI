13b006f0aa6097f8bf3d3c259e479df21ced7c1d
RyanEggert <Ryan.Eggert@students.olin.edu>
2014-03-10 00:59:34 -0400

    More Comments & Cleanup
    More comments. Removed unused time call

--- ProductLookup.py
+++ ProductLookup.py
@@ -1,27 +1,28 @@
 # ProductLookup.py
 
 import bottlenose  # Amazon P.A. API bindings
-import string
+import string  # String containing ASCII letters
 import xlwt  # Write to MS Excel file
 import time  # Schedule data collection
 
 
 def ASINLookup(ASIN, x, y):  # x is looping through ASINs. y for time
-    amazon = bottlenose.Amazon(
-        'AKIAI4I6EMMUJQXSGCFA', '/QTXbjXBMJk75UsOxNbAhRwkk0aVjPAKUla+ofaM',
-        'ryane-20')
+    """Looks up a given ASIN. Uses given indices to place value(s)
+    in an existing table"""
+    amazon = bottlenose.Amazon('AMAZON_ACCESS_KEY_ID',
+                               ' AMAZON_SECRET_KEY', 'AMAZON_ASSOC_TAG')
 
-    # Rapid requests periodically fail. Code reattempts failed requests a set 
-    # number of times. This number has been tested, and it minimizes the number  
+    # Rapid requests periodically fail. Code reattempts failed requests a set
+    # number of times. This number has been tested, and it minimizes the number
     # of valueless returns while not devoting too much time to repeat requests.
     for attempts in range(1, 10):
         try:
             # Attempt API call
-            product = amazon.ItemLookup( 
-                ItemId=ASIN, ResponseGroup='SalesRank,ItemAttributes') 
+            product = amazon.ItemLookup(
+                ItemId=ASIN, ResponseGroup='SalesRank,ItemAttributes')
         except:
-            print "HTML/Internet Connection Error" #Display error msg.
-            time.sleep(.5) #Wait before retrying
+            print "HTML/Internet Connection Error"  # Display error msg.
+            time.sleep(.5)  # Wait before retrying
 
         else:
             Response = str(product)
@@ -51,13 +52,13 @@ def ASINLookup(ASIN, x, y):  # x is looping through ASINs. y for time
                 SalesRank = SR.translate(None, IsolateSalesRank)
                 # strip all chars, except for digits, leaving only the
                 # SalesRank behind
-            print SalesRank
+            print SalesRank  # Display SalesRank for progress checking
 
             try:
                 Sheet.write(x + 1, 1 + y, int(SalesRank))
             except ValueError:
-                Sheet.write(x + 1, 1 + y, SalesRank)
-            break #Leave loop on successful product loopkup process
+                Sheet.write(x + 1, 1 + y, SalesRank)  # Write 'Unranked'
+            break  # Leave loop on successful product loopkup process
     else:  # Multiple Attempts have failed
         print "Fiddlesticks."  # Oh bother.
         if y == 1:  # Only find category the first time
@@ -66,39 +67,39 @@ def ASINLookup(ASIN, x, y):  # x is looping through ASINs. y for time
             Sheet.write(x + 1, 1, "Internet Error")
         Sheet.write(x + 1, 1 + y, "Internet Error")
 
-
-OrigTime = time.strftime('%c %z', time.localtime())
+# Use time to dynamically name file--prevent overwrite
 FileStart = time.strftime('%m%d_%H%M', time.localtime())
-ASINText = open('ASINsFin.txt', 'r')
+ASINText = open('ASINsFin.txt', 'r')  # Open text 'list' of products to lookup
 
 RwIn = ASINText.read()  # Raw Input
 
-ASINs = RwIn.split('\n') # Split into list of ASINs
+ASINs = RwIn.split('\n')  # Split into list of ASINs
 
 
 wbk = xlwt.Workbook()  # Create workbook object
 Sheet = wbk.add_sheet('Product Sales Rank Data', cell_overwrite_ok=True)
 
-#Column Headers
-Sheet.write(0, 0, "ASIN") 
+# Column Headers
+Sheet.write(0, 0, "ASIN")
 Sheet.write(0, 1, "Category")
 
-IsolateSalesRank = string.ascii_letters + '<>/'
-IsolateCategory = '<>/'
+IsolateSalesRank = string.ascii_letters + '<>/'  # To remove all letters & <>/
+IsolateCategory = '<>/'  # Used to remove all < , > , /. Similar to above.
 
-RunT = 40 #Number of hours to check.
-for j in range(1, RunT+1):  # RunT+1 to make range() inclusive. First j always 1
+RunT = 40  # Number of hours to check.
+# RunT+1 to make range() inclusive. First j always 1
+for j in range(1, RunT + 1):
     startTime = time.strftime('%c %z', time.localtime())
-    Sheet.write(0, j + 1, "Sales Rank" + startTime)
-    eTs = time.clock()
+    Sheet.write(0, j + 1, "Sales Rank" + startTime)  # Timestamp rank columns
+    eTs = time.clock()  # 'Elapsed Time' Start
     for i in range(1, len(ASINs) + 1):
         ASINLookup(ASINs[i - 1], i, j)
         print "Searching Product " + str(i) + " of " + str(len(ASINs))
         # time.sleep(.5) # Not needed. Pause only on fail condition
 
-    #Dynamically name file to prevent overwrite
-    wbk.save('AmazonSalesRankData' + FileStart + '.xls') 
-    eTf = time.clock()
+    # Dynamically name file to prevent overwrite
+    wbk.save('AmazonSalesRankData' + FileStart + '.xls')
+    eTf = time.clock()  # 'Elapsed Time' Finish
     elapsedTime = eTf - eTs  # Elapsed time of each loop iteration
     print 'Elapsed Time = ' + str(elapsedTime)
     if elapsedTime < 3600:
